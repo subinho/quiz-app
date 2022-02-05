88bd93f915b0e41a86a2778af44855a1c428ef30
Stepan Subrt <stepansubrt@icloud.com>
2022-02-05 21:20:28 +0100

    build: shuffling answers

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-export const Quiz = ({data: {question, correct_answer, incorrect_answers}}) => {
+export const Quiz = ({data: {question, correct_answer, incorrect_answers} })=> {
+  const shuffledAnswers = [correct_answer, ...incorrect_answers].sort(() => Math.random * 0.5);
+
   return (
     <div className='quiz'>
         <div className='quiz-question'>
             <h2>{question}</h2>    
         </div>
         <div className='quiz-answers'>
-            <button className='quiz-answers-button'>{correct_answer}</button>
-            <button className='quiz-answers-button'>{incorrect_answers[0]}</button>
-            <button className='quiz-answers-button'>{incorrect_answers[1]}</button>
-            <button className='quiz-answers-button'>{incorrect_answers[2]}</button>
+            <button className='quiz-answers-button'>{shuffledAnswers[0]}</button>
+            <button className='quiz-answers-button'>{shuffledAnswers[1]}</button>
+            <button className='quiz-answers-button'>{shuffledAnswers[2]}</button>
+            <button className='quiz-answers-button'>{shuffledAnswers[3]}</button>
         </div>
   </div>
   );
