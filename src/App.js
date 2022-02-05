4c1da5f228b9ff58b0cf669db3212aa1476fe97e
Stepan Subrt <stepansubrt@icloud.com>
2022-02-05 20:46:08 +0100

    fix: decoding data from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import he from 'he';
 
 import { Welcome } from './components/Welcome';
 import { Quiz } from './components/Quiz';
 
   const App = () => { 
     const [startQuiz, setStartQuiz] = useState(false);
-    const [questions, setQuestions] = useState([])
+    const [questions, setQuestions] = useState([]);
 
     const api_url = 'https://opentdb.com/api.php?amount=10&type=multiple'
 
     useEffect(async () => {
-      const response = await fetch(api_url)
-      const data = await response.json()
+      const response = await fetch(api_url);
+      const data = await response.json();
+      const decodedData = data.results.map(result => {
+        return {
+          ...result,
+          question: he.decode(result.question),
+          correct_answer: he.decode(result.correct_answer),
+          incorrect_answers: result.incorrect_answers.map(incorrect => he.decode(incorrect))
+        }
+      })
 
-      setQuestions(data.results)
+      setQuestions(decodedData)
     }, [])
 
     const handleStart = () => {
