7c66681bdac7e17cc142b5074df66ad97aea89c4
Stepan Subrt <stepansubrt@icloud.com>
2022-02-28 21:11:00 +0100

    build: moved options to separate file

---
+++ src/Data.js
@@ -0,0 +1,40 @@
+export const amountOfQuestions = [
+    {value: '5', label: '5'},
+    {value: '10', label: '10'},
+    {value: '15', label: '15'}
+]
+
+export const difficulty = [
+    {value: 'Any Difficulty', label: 'Any Difficulty'},
+    {value: 'Easy', label: 'Easy'},
+    {value: 'Medium', label: 'Medium'},
+    {value: 'Hard', label: 'Hard'}
+]
+
+export const category = [
+    {value: 'Any Category', label: 'Any Category'},
+    {value: 'General Knowledge', label: 'General Knowledge'},
+    {value: 'Entertainment: Books', label: 'Entertainment: Books'},
+    {value: 'Entertainment: Film', label: 'Entertainment: Film'},
+    {value: 'Entertainment: Music', label: 'Entertainment: Music'},
+    {value: 'Entertainment: Musicals & Theaters', label: 'Entertainment: Musicals & Theaters'},
+    {value: 'Entertainment: Televison', label: 'Entertainment: Televison'},
+    {value: 'Entertainment: Video Games', label: 'Entertainment: Video Games'},
+    {value: 'Entertainment: Board Games', label: 'Entertainment: Board Games'},
+    {value: 'Science & Nature', label: 'Science & Nature'},
+    {value: 'Science: Computers', label: 'Science: Computers'},
+    {value: 'Science: Mathematics', label: 'Science: Mathematics'},
+    {value: 'Mythology', label: 'Mythology'},
+    {value: 'Sports', label: 'Sports'},
+    {value: 'Geography', label: 'Geography'},
+    {value: 'History', label: 'History'},
+    {value: 'Politics', label: 'Politics'},
+    {value: 'Art', label: 'Art'},
+    {value: 'Celebrities', label: 'Celebrities'},
+    {value: 'Animals', label: 'Animals'},
+    {value: 'Vehicles', label: 'Vehicles'},
+    {value: 'Entertainment: Comics', label: 'Entertainment: Comics'},
+    {value: 'Science: Gadgets', label: 'Science: Gadgets'},
+    {value: 'Entertainment: Japanese Anime & Manga', label: 'Entertainment: Japanese Anime & Manga'},
+    {value: 'Entertainment: Cartoon & Animations', label: 'Entertainment: Cartoon & Animations'}
+]
